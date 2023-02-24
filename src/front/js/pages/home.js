21efeb735bc95c18288b4994ebb2dd734b44b276
TheoYanvno <TheoYanvno@gmail.com>
2023-02-24 23:18:55 +0000

    map update

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -8,8 +8,8 @@ export const Home = () => {
 
 	return (
 		<div className="HomePage">
-			<div className="map" style={{width:'100%', height:'100vh'}}>
-				<iframe src="https://www.google.com/maps/embed/v1/place?key=__&q=Space+Needle+WA" style={{width:'100%', height:'100vh', border:'0', frameBorder:'0'}}></iframe>
+			<div className="map" style={{width:'100%', height:'80%'}}>
+				<iframe src="https://www.google.com/maps/embed/v1/place?key=AIzaSyCiBrtiZK1x072QJ5-d1d8fWXmJhfZSkrs&q=FL" style={{width:'100%', height:'100vh', border:'0', frameBorder:'0'}}></iframe>
 			</div>
 		</div>
 	);
